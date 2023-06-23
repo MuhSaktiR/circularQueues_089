cdff508cc830fa5b168d06dad83163eefc99de7c
MuhSaktiR <seken876@gmail.com>
2023-06-23 15:01:29 +0700

    menambahkan if cek antrian kosong dan else jika rear berada di posisi terakhir array dengan step 4 queue_array[REAR] = num

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -19,8 +19,22 @@ public:
 
 		//CEk apakah antrian penuh
 		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
-
+			cout << "\nQueue Overflow\n";
+			return;
 		}
-	}
 
+		//Cek Apakah antrian Kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//Jika REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
+	}
 };
